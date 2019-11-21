eccd129c14c35024a372015fa7378a4e4fa20aff
mswanson-mscst <mswanson@southeastmn.edu>
2019-11-21 09:20:57 -0600

    Added home and navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule,
+  MatCardModule, MatFormFieldModule, MatInputModule,
+  MatDialogModule, MatGridListModule
+} from '@angular/material';
+
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule,
+    MatCardModule, MatFormFieldModule, MatInputModule,
+    MatDialogModule, MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
